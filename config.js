daa82b12b26b74bf408bb4f1795ad8b63418611f
Phenciab <you@example.comphenciabalou@gmail.com>
2019-04-07 15:58:03 +0000

    projet alimentation

---
+++ config.js
@@ -0,0 +1,12 @@
+var config = {
+    database: {
+        host: 'localhost',
+        user: 'root',
+        password: '',
+        port: 3306,
+        db: 'devorbd',
+        secret: 'commando leger'
+    }
+}
+
+module.exports = config
